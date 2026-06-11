936764f8f72c95de1e15c8bd3d8a388b7513c559
Sandeep <sandeepchauhan43215@gmail.com>
2026-06-11 12:57:46 +0530

    Add files via upload

---
+++ Week_2/Day_2/LongestSubStringWithoutRepeatingCharacters.java
@@ -0,0 +1,22 @@
+package Day_2;
+
+public class LongestSubStringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int[] freq = new int[128]; // ASCII
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            freq[ch]++;
+
+            while (freq[ch] > 1) {
+                freq[s.charAt(left)]--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
